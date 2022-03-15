0b9d8d449b75f782638054d28964661dc8413a46
Jason Au <jxa2009@rit.edu>
2022-03-15 12:13:10 -0400

    clarified variable names

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import psycopg2
 from sshtunnel import SSHTunnelForwarder
 
 
-db_username = ""
-db_password = ""
+cs_username = ""
+cs_password = ""
 dbName = "p320_18"
 
 
@@ -11,15 +11,18 @@ dbName = "p320_18"
 # create a login_info.txt file with two lines: a line containing your username on the first and password on the second
 def get_login_info():
     f = open("login_info.txt","r")
-    global db_username, db_username
-    db_username = f.readline().strip()
-    db_username = f.readline().strip()
+    global cs_username, cs_username
+    cs_username = f.readline().strip()
+    cs_username = f.readline().strip()
     
 def command_parser():
     print("Welcome to the tools management system. Please login:")
 
     # query usernames and passwords and make sure it exists
-
+    while True:
+        if account_exists():
+            break
+        
 
 
 #function that queries username and password from the database
@@ -33,15 +36,15 @@ def main():
     get_login_info()
     try:
         with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
-                                ssh_username=db_username,
-                                ssh_password=db_username,
+                                ssh_username=cs_username,
+                                ssh_password=cs_username,
                                 remote_bind_address=('localhost', 5432)) as server:
             server.start()
             print("SSH tunnel established")
             params = {
                 'database': dbName,
-                'user': db_username,
-                'password': db_username,
+                'user': cs_username,
+                'password': cs_username,
                 'host': 'localhost',
                 'port': server.local_bind_port
             }
