0a9cee2a7890a06708a84c1350d93d8a30934fa4
Jason Au <jxa2009@rit.edu>
2022-03-15 12:05:46 -0400

    added way to login to database

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 import psycopg2
 from sshtunnel import SSHTunnelForwarder
-import base64
 
 
-username = ""
-password = ""
+db_username = ""
+db_password = ""
 dbName = "p320_18"
 
+
+
+# create a login_info.txt file with two lines: a line containing your username on the first and password on the second
 def get_login_info():
     f = open("login_info.txt","r")
-    global username, password
-    username = f.readline().strip()
-    password = f.readline().strip()
+    global db_username, db_username
+    db_username = f.readline().strip()
+    db_username = f.readline().strip()
     
-    
+def command_parser():
+    print("Welcome to the tools management system. Please login:")
+
+    # query usernames and passwords and make sure it exists
+
+
+
+#function that queries username and password from the database
+# returns true if exists, false else
+def account_exists():
+    username = input("Username: ")
+    password = input("Password: ")
+
+
 def main():
     get_login_info()
     try:
         with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
-                                ssh_username=username,
-                                ssh_password=password,
+                                ssh_username=db_username,
+                                ssh_password=db_username,
                                 remote_bind_address=('localhost', 5432)) as server:
             server.start()
             print("SSH tunnel established")
             params = {
                 'database': dbName,
-                'user': username,
-                'password': password,
+                'user': db_username,
+                'password': db_username,
                 'host': 'localhost',
                 'port': server.local_bind_port
             }
@@ -37,6 +52,7 @@ def main():
 
             print("Database connection established")
 
+            command_parser()
 
             
             conn.close()
