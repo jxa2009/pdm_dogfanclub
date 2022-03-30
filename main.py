584cf3c0b7fa6e0786488084428a5c093712dcc0
sjs7128 <sjs7128@rit.edu>
2022-03-30 18:51:12 -0400

    Added documentation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
+"""
+Tools Domain Main file
+"""
 
+import datetime
 import psycopg2
 from sshtunnel import SSHTunnelForwarder
 from datetime import date
@@ -10,8 +13,19 @@ dbName = "p320_18"
 current_username = ""
 
 
+
 # create a login_info.txt file with two lines: a line containing your username on the first and password on the second
 def get_login_info():
+    """
+    Gets the login info from 'login_info.txt'
+    to log into the database
+
+    Arguments:
+        None
+    
+    Returns:
+        None
+    """
     f = open("login_info.txt", "r")
     global cs_username, cs_password
     cs_username = f.readline().strip()
@@ -19,14 +33,36 @@ def get_login_info():
     return
 
 
+
 def command_parser(curs):
+    """
+    Is called by the main function and calls
+    both of the command parser functions
+
+    Arguments:
+        curs: the connection cursor
+
+    Returns
+        None
+    """
     print("Welcome to the tools management system")
     # query usernames and passwords and make sure it exists
     login_user(curs)
     run_program(curs)
 
 
+
 def login_user(curs):
+    """
+    While loop used parsing input to create a new user
+    or let an existing user login
+
+    Arguments:
+        curs: the connection cursor
+
+    Returns:
+        None
+    """
     while True:
         print(
             "usage:\n\tcreate a new account: create [username] [password] [first name] [last name] [email]\n\tlogin [username] [password]")
@@ -73,15 +109,26 @@ def login_user(curs):
             print("invalid command")
 
 
+
 def run_program(curs):
+    """
+    While loop that parses a user's commands and 
+    calls the correct functions for these commands
+
+    Arguments:
+        curs:   the connection cursor
+
+    Returns:
+        None
+    """
     while True:
         # print the command you're gonna add here and how to use it
 
         print("usage:")
         print("\tcategory new [category_name]")
         print("\t         add [tool_barcode] [category_id]")
-        print("\n\tsearch barcode [tool_barcode]")
-        print("\t       name [tool_name]")
+        print("\n\tsearch barcode  [tool_barcode]")
+        print("\t       name     [tool_name]")
         print("\t       category [tool_category]")
         print("\n\tdelete barcode [tool_barcode]")
         print("\n\tborrow barcode [tool_barcode]")
@@ -104,6 +151,7 @@ def run_program(curs):
                     print("added category")
                 else:
                     print("failed to add category")
+
             elif sub_action == "add" and cmd_sz == 4:
                 tool_barcode = parsed_cmd[2]
                 category_id = parsed_cmd[3]
@@ -138,13 +186,16 @@ def run_program(curs):
                 barcode = parsed_cmd[2]
                 delete_tools_by_barcode(curs, barcode)
                 print("Successfully deleted")
+
         elif action == "borrow" and cmd_sz > 2:
             sub_action = parsed_cmd[1]
             if sub_action == "barcode":
                 barcode = parsed_cmd[2]
+                # This function does nothing
                 borrow_tools(curs, barcode)
                 update_request(curs, barcode)
                 print("Successfully made the request")
+
         elif action == "modify":
             sub_action = parsed_cmd[1]
 
@@ -193,6 +244,7 @@ def run_program(curs):
 
                 else:
                     print("invalid command")
+
             elif sub_action == "delete":
                 print("type the barcode of the tool you would like to delete(barcode)")
                 cmd2 = input()
@@ -209,18 +261,24 @@ def run_program(curs):
 
             else:
                 print("invalid command")
+
         elif (action == "exit"):
             break
         else:
             print("invalid command")
 
+
+
 def update_request(curs, barcode):
     #Run insert query when borrow request is made
     try:
+        # The username is already a global variable
         print("Your Username?")
         cmd = input()
         print("Date Required?")
         cmd1 = input(datetime.date)
+
+        # The tool hasn't been returned yet so we don't know the return date
         print("Date Returned?")
         cmd2 = input(datetime.date)
         print("Duration?")
@@ -232,51 +290,107 @@ def update_request(curs, barcode):
         params = (int(barcode), )
         curs.execute(query2, params)
         res = curs.fetchall()
-        status = 'pending'
+        status = 'Pending'
         params2 = (int(barcode), cmd, str(res), status, cmd1, cmd2, int(cmd3),)
         curs.execute(query1, params2)
-    except:
+    except Exception as e:
+        print(e)
         print("update_request failure")
         return False
     return True
 
+
+
 def edit_new_toolname(curs, toolbarcode, newtoolname):
+    """
+    Updates the name of a user's tool
+
+    Arguments:
+        curs:           the connection cursor
+        toolbarcode:    the barcode of the tool to be edited
+        newtoolname:    the new tool name
+
+    Returns:
+        True:           if successfully updated the tool's name
+        False:          if failed to update the tool's name
+    """
     global current_username
     try:
         query = "UPDATE p320_18.\"Tools\" SET \"Tool Name\" = %s WHERE p320_18.\"Tools\".\"Tool Barcode\" = %s AND p320_18.\"Tools\".\"Username\" = %s;"
         params = (newtoolname, toolbarcode, current_username,)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("edit_new_toolname failure")
         return False
     return True
 
 
+
 def edit_shareable(curs, toolbarcode, newshareable):
+    """
+    Updates the Shareable attribute of a user's tool
+
+    Arguments:
+        curs:           the connection cursor
+        toolbarcode:    the barcode of the tool to be edited
+        newshareable:   the sharable attributte ('Shareable' or 'Unshareable' )
+
+    Returns:
+        True:           if succeffully updated the Shareable attribiute
+        False:          if failed to update the Shareable attribiute
+    """
     global current_username
     try:
         query = "UPDATE p320_18.\"Tools\" SET \"Shareable\" = %s WHERE p320_18.\"Tools\".\"Tool Barcode\"  = %s AND p320_18.\"Tools\".\"Username\" = %s;"
         params = (newshareable, toolbarcode, current_username,)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("edit_shareable failure")
         return False
     return True
 
 
+
 def edit_description(curs, toolbarcode, newdescription):
+    """
+    Updates the description of a user's tool with a new description
+
+    Arguments:
+        curs:           the connection cursor
+        toolbarcode:    the barcode of the tool to be edited
+        newdescription: the new description
+
+    Returns:
+        True:           if succeffully updated the description
+        False:          if failed to update the description
+    """
     global current_username
     try:
         query = "UPDATE p320_18.\"Tools\" SET \"Description\" = %s WHERE p320_18.\"Tools\".\"Tool Barcode\" = %s AND p320_18.\"Tools\".\"Username\"= %s ;"
         params = (newdescription, toolbarcode, current_username,)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("edit_description failure")
         return False
     return True
 
 
+
 def delete_new_toolname(curs, toolbarcode):
+    """
+    Removes a tool form a user's catalog
+
+    Arguments:
+        curse:          the connection cursor
+        toolbarcode:    the barcode of the tool to be removed
+
+    Returns:
+        True:           if the tool was successfully removed for the user's catalog
+        False:          if failed to remove the tool from the user's catalog
+    """
     global current_username
     try:
         query = "UPDATE p320_18.\"Tools\" SET \"Username\" = NULL WHERE p320_18.\"Tools\".\"Tool Barcode\" = %s AND p320_18.\"Tools\".\"Username\" = %s ;"
@@ -289,19 +403,45 @@ def delete_new_toolname(curs, toolbarcode):
     return True
 
 
+
 def add_new_toolname_User(curs, toolbarcode):
+    """
+    Adds a new tool to a user's catalog if that tool is not already owned
+
+    Arguments:
+        curs:           the connection cursor
+        toolbarcode:    the barcode of the tool to be added
+
+    Returns:
+        True:           if the tool was successfully added to a user's catalog
+        False:          if failed to add a tool to the user's catalog
+    """
     global current_username
     try:
         query = "UPDATE p320_18.\"Tools\" SET \"Username\" = %s WHERE p320_18.\"Tools\".\"Tool Barcode\" = %s AND  p320_18.\"Tools\".\"Username\" IS NULL ;"
         params = (current_username, toolbarcode,)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("add_new_toolname_User failure")
         return False
     return True
 
 
+
+# This might need a (try: except:) around the query
 def category_id_exists(curs, category_id):
+    """
+    Determnes if a category ID exists in the database
+
+    Arguments:
+        curs:           the connection cursor
+        category_id:    the ID of he category to look for
+
+    Returns:
+        True:           if the category ID exists
+        False:          if the category ID does not exist
+    """
     query = "SELECT \"Category ID\" FROM p320_18.\"Categories\""
     curs.execute(query)
     res = curs.fetchall()
@@ -312,7 +452,20 @@ def category_id_exists(curs, category_id):
     return False
 
 
+
+# This might need a (try: except:) around the query
 def category_name_exists(curs, category_name):
+    """
+    Determines if a category exists in the database
+
+    Arguments:
+        curse:          the connection cursor
+        category_name:  the name of the category to search for
+
+    Returns:
+        True:           if the category name exists in the database
+        False:          if the category name does not exist in the database
+    """
     # SELECT "Category Name" FROM p320_18."Categories" check if category exists before trying to insert
     query = "SELECT \"Category Name\" FROM p320_18.\"Categories\""
     curs.execute(query)
@@ -324,8 +477,19 @@ def category_name_exists(curs, category_name):
     return False
 
 
-# return all the categories a tool belongs to
+
+# This might need a (try: except:) around the query
 def get_tools_categories(curs, tool_barcode):
+    """
+    Returns a list of all the categories a tool belongs to
+
+    Arguments:
+        curs:           the connection cusor
+        tool_barcode:   the barcode of the specified tool
+
+    Returns:
+        The list of all the categories a tool belongs to
+    """
     query = "SELECT \"Category ID\" FROM p320_18.\"Tool Categories\" WHERE p320_18.\"Tool Categories\".\"Tool Barcode\" = %s"
     params = (tool_barcode,)
 
@@ -336,7 +500,20 @@ def get_tools_categories(curs, tool_barcode):
     return res
 
 
+
 def add_category_to_tool(curs, tool_barcode, category_id):
+    """
+    Adds the specified category to the tool with the specified barcode
+
+    Arguments:
+        curs:           the connection cursor
+        tool_barcode:   the barcode of the tool to add the category to
+        category_id:    the ID of category to add to the tool
+
+    Returns:
+        True:           if the category was successfully added to the tool
+        False:          if failed adding the category to the tool
+    """
     if not category_id_exists(curs, category_id):
         return False
 
@@ -347,19 +524,32 @@ def add_category_to_tool(curs, tool_barcode, category_id):
         if category_id == str(curr_id):
             print("tool is already in category")
             return False
-
-    # make sure tool already not allocated to the caategory
+    # make sure tool already not allocated to the category
     try:
         query = "INSERT INTO p320_18.\"Tool Categories\"(\"Tool Barcode\",\"Category ID\") VALUES (%s, %s)"
         params = (tool_barcode, category_id,)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("add_category_tool failure")
         return False
     return True
 
 
+
 def add_new_category(curs, name):
+    """
+    Adds the specified category to the database
+
+    Arguments:
+        curs:   the connection cursor
+        name:   the name of the category to be added
+
+    Returns:
+        True:   if category was succefully added
+        False:  if failed to add category
+    """
+    # check if category exists before trying to insert
     if category_name_exists(curs, name):
         return False
 
@@ -375,18 +565,33 @@ def add_new_category(curs, name):
         curs.execute(query, params)
 
         print("added category \"{category_name}\" with id \"{id}\"".format(category_name=name, id=category_id))
-    except:
+    except Exception as e:
+        print(e)
         print("add_new_category: failed query")
         return False
     return True
 
 
+
+# This only prints the name of the tool found. It should print more data about the tool
 def find_tool_by_barcode(curs, barcode):
+    """
+    Finds and prints the name of the tool that has the specified barcode
+
+    Arguments:
+        curs:       the connection cursor
+        barcode:    the barcode to search for
+
+    Returns:
+        True:   if successfully seached for a tool by barcode
+        False:  if failed to search for a tool by barcode
+    """
     try:
         query = "SELECT \"Tool Name\" FROM p320_18.\"Tools\" WHERE \"Tool Barcode\" = %s;"
         params = (int(barcode),)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("FIND_TOOL_BY_BARCODE FAILED QUERY")
         return False
 
@@ -398,34 +603,55 @@ def find_tool_by_barcode(curs, barcode):
     return True
 
 
+
+# This deletes a tool from the database rather than deleting the tool form the user's catalog
+# We don't want a user to be able to do this
 def delete_tools_by_barcode(curs, barcode):
     try:
         query = "DELETE FROM p320_18.\"Tools\" WHERE \"Tool Barcode\" = %s AND \"Username is null\";"
         params = (int(barcode),)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("DELETE_TOOLS_BY_BARCODE QUERY FAILED")
         return False
     return True
 
 
+
+# This selects a tool barcode but does nothing with it
 def borrow_tools(curs, barcode):
     try:
         query = "SELECT \"Tool Barcode\" FROM p320_18.\"Tools\" WHERE \"Shareable\" = 'Shareable';"
         params = (int(barcode),)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("BORROW_TOOLS QUERY FAILED")
         return False
     return True
 
 
+
+# This only prints the name of the tools found. It should print more data about the tools
 def find_tool_by_name(curs, name):
+    """
+    Finds and prints the name of every tool that contains the specified string in the tool's name
+
+    Arguments:
+        curs:   the connection cursor
+        name:   the string to search for
+
+    Returns:
+        True:   if successfully seached for tools by name
+        False:  if failed to search for tools by name
+    """
     try:
         query = "SELECT \"Tool Name\" FROM p320_18.\"Tools\" WHERE \"Tool Name\" LIKE %s ORDER BY \"Tool Name\" ASC;"
         params = ('%' + name + '%',)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("FIND_TOOL_BY_NAME FAILED QUERY")
         return False
 
@@ -439,12 +665,26 @@ def find_tool_by_name(curs, name):
     return True
 
 
+
+# This only prints the name of the tools found. It should print more data about the tools
 def find_tool_by_category(curs, category):
+    """
+    Finds and print the name of all tools of a specified category
+
+    Arguments:
+        curs:       the connection cursor
+        category:   the category to be searched for
+    
+    Returns:
+        True:       if successfully search for a category
+        False:      if failed to search for a category
+    """
     try:
         query = "SELECT \"Tool Name\" FROM p320_18.\"Tools\" WHERE \"Tool Barcode\" IN (SELECT \"Tool Barcode\" FROM  p320_18.\"Tool Categories\" WHERE \"Category ID\" = (SELECT \"Category ID\" FROM p320_18.\"Categories\" WHERE \"Category Name\" = %s)) ORDER BY \"Tool Name\" ASC;"
         params = (category,)
         curs.execute(query, params)
-    except:
+    except Exception as e:
+        print(e)
         print("FIND_TOOL_BY_CATEGORY FAILED QUERY")
         return False
 
@@ -458,7 +698,23 @@ def find_tool_by_category(curs, category):
     return True
 
 
+
 def create_user(curs, user, pwd, f_name, l_name, email):
+    """
+    Creates a new user and adds it to the database
+
+    Arguments:
+        curs:   the connection cursor
+        user:   the username of the new user
+        pwd:    the password of the new user
+        f_name: the firstname of the new user
+        l_name: the last name of the new user
+        email:  the email of the new user
+
+    Returns:
+        True:   if successfully created a new user
+        False:  if faild creating a new user
+    """
     if user_exists(curs, user, pwd):
         print("user already exists in data base")
         return False
@@ -473,19 +729,34 @@ def create_user(curs, user, pwd, f_name, l_name, email):
     try:
         curs.execute(query, params)
 
-    except:
+    except Exception as e:
+        print(e)
         print("CREATE_USER FAILED QUERY")
         return False
 
     return True
 
 
-# returns true if user exists in data base.
+
 def user_exists(curs, user, pwd):
+    """
+    Returns True if the selected user is in the database
+    Returns False otherwise
+
+    Arguments:
+        curs:   the connection cursor
+        user:   the username of the user selected
+        pwd:    the password of the user selected
+
+    Returns:
+        True:   if user is in the database
+        False:  if user is not in the database
+    """
     query = "SELECT \"Username\", \"Password\" FROM p320_18.\"User\""
     try:
         curs.execute(query)
-    except:
+    except Exception as e:
+        print(e)
         print("USER_EXISTS FAILED QUERY")
         return False
     res = curs.fetchall()
@@ -495,7 +766,18 @@ def user_exists(curs, user, pwd):
     return False
 
 
+
 def main():
+    """
+    Main function for the program
+    Initializes the connection to the database
+
+    Arguemnts:
+        None
+
+    Returns:
+        None
+    """
     get_login_info()
 
     try:
